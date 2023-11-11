abfec52390ba0bbd5633b7b103985b84e8fdbbbe
117Isabell <isabellhepburn@outlook.com>
2023-11-11 20:11:52 +0100

    Add the winner checking logic

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 let squares = document.querySelectorAll(".square");
 let boardArray = ["", "", "", "", "", "", "", "", ""];
 let firstPlayer = "X";
+let winningMessage = document.createElement("p");
+let body = document.getElementById("body");
+console.log(body);
+
+const winComs = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 // let secondPlayer = "O";
 // let currentPlayer = firstPlayer;
 
@@ -26,4 +40,28 @@ function handleSquare(clickedSquareIndex, clickedSquare) {
   clickedSquare.innerHTML = firstPlayer;
 
   firstPlayer = firstPlayer === "X" ? "O" : "X";
+  checkWins();
 }
+
+function checkWins() {
+  console.log(boardArray);
+  winComs.forEach((winCom) => {
+    {
+      if (
+        (boardArray[winCom[0]] !== "" && boardArray[winCom[0]]) ===
+          (boardArray[winCom[1]] !== "" && boardArray[winCom[1]]) &&
+        boardArray[winCom[1]] !== "" &&
+        (boardArray[winCom[1]] === boardArray[winCom[2]]) !== "" &&
+        boardArray[winCom[2]]
+      ) {
+        // alert(`The winner is ${boardArray[winCom[0]]} `);
+        winningMessage.innerHTML = `The winner is ${boardArray[winCom[0]]} `;
+        body.appendChild(winningMessage);
+      }
+    }
+  });
+}
+
+// [[0,1,2], [3,4,5]......]
+// 1st iteratrion ->  [0,1,2,]  board[0] => X loop through all values in wincom= [1,2,3] and find if there is a match
+// 2nd iteration ->
